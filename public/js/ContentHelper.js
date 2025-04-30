cd4409d5f58388940ba6ac39529fce68a99cb121
NikkieDev <nikkie@schaad.zip>
2025-04-30 15:58:05 +0200

    commit

---
+++ public/js/ContentHelper.js
@@ -0,0 +1,4 @@
+class ContentHelper
+{
+    constructor() {}
+}
---
+++ public/js/TagHelper.js
@@ -0,0 +1,43 @@
+class TagHelper
+{
+    constructor()
+    {
+        this.title = document.querySelector('.content-title');
+        this.tags = Array.from(document.querySelectorAll('.tag')).map(t => ({
+            element: t,
+            target: document.querySelector(t.getAttribute('data-target')),
+            name: t.textContent
+        }));
+
+        this.setContent = this.setContent.bind(this)
+
+        this.tags.forEach(t => t.element.addEventListener('click', this.setContent));
+    }
+
+    findTag(element)
+    {
+        return this.tags.find(t => t.element == element);
+    }
+
+    getContent()
+    {
+        return document.querySelector('.tag-content.active');
+    }
+
+    setContent(event)
+    {
+        const clickedTag = this.findTag(event.target);
+        const currentActive = this.getContent();
+
+        currentActive.classList.remove('active');
+        clickedTag.target.classList.add('active');
+        this.setTitle(clickedTag);
+    }
+
+    setTitle(tag)
+    {
+        this.title.textContent = tag.name
+    }
+}
+
+new TagHelper();
