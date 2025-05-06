e44a813756c9b0cd252f11e84de13c80186b0970
Nikkie Schaad <nikkie@newaspect.nl>
2025-05-06 10:31:08 +0200

    fix not returning

--- Util/TLDParser.js
+++ Util/TLDParser.js
@@ -3,9 +3,10 @@ class TLDParser
     static getDomain(url)
     {
         let domain = '';
-
+        
         try {
             domain = new URL(url).hostname;
+            return domain;
         } catch (error) {
             return domain;
         }
